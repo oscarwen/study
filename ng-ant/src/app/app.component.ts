27c63de6d9e03bdb4de47450bdb93e5d7390919c
oscarwen <hzwenli@163.com>
2019-05-21 22:04:20 +0800

    OK

--- ng-ant/src/app/app.component.ts
+++ ng-ant/src/app/app.component.ts
@@ -2,8 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div nz-row>
+      <div nz-col nzSpan="12">col-12</div>
+      <div nz-col nzSpan="12">col-12</div>
+    </div>
+    <div nz-row>
+      <div nz-col nzSpan="8">col-8</div>
+      <div nz-col nzSpan="8">col-8</div>
+      <div nz-col nzSpan="8">col-8</div>
+    </div>
+    <div nz-row>
+      <div nz-col nzSpan="6">col-6</div>
+      <div nz-col nzSpan="6">col-6</div>
+      <div nz-col nzSpan="6">col-6</div>
+      <div nz-col nzSpan="6">col-6</div>
+    </div>
+  `
+  /* templateUrl: './app.component.html', */
+  /* styleUrls: ['./app.component.css'] */
 })
 export class AppComponent {
   title = 'ng-ant';
